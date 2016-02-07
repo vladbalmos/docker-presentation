1046f8ba5cc0189d7a5629e6ed9c75d473b81df9
Vlad Balmos <vlad.balmos@gmail.com>
2016-02-07 16:04:08 +0200

    More slides and start work on wordpress practical example

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 mkdir -p ~/tmp/docker-presentation/host-volume
+
+if [ ! -d wordpress-4.3 ]; then
+    git clone https://github.com/vladbalmos/WordPress.git wordpress-4.3
+    cd wordpress-4.3
+    git checkout tags/4.3
+    rm -rf .git
+fi
